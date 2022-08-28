193f2bb92999c84b77b14a3b81612284b6e8f0ed
ferTaipi <fernandoparadoja@gmail.com>
2022-08-27 20:44:27 -0500

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import quotes from "./componentes/quotes.json"
 import QuoteBox from './componentes/QuoteBox'
-const colors = ["black", "blue", "green","#0E0A14", "#494454", "#032720", "#32554C", "#251F34", "#4B4261", "#220200", "#38210D"]
 
+const colors = ["black", "blue", "green", "#0E0A14", "#494454", "#032720", "#32554C", "#251F34", "#4B4261", "#220200", "#38210D"]
 function App() {
-
   const randomIndex = Math.floor(Math.random() * quotes.length)
   const [index, setIndex] = useState(randomIndex)
   const changerUser = () => {
@@ -15,10 +14,10 @@ function App() {
   const randomColor = Math.floor(Math.random() * colors.length)
   document.body.style = `background:${colors[randomColor]}`
   return (
-     <div> 
-    <div className="App">
-      <QuoteBox quotes={quotes} changerUser={changerUser} index={index} colors={colors} randomColor={randomColor} />
-    </div>
+    <div>
+      <div className="App">
+        <QuoteBox quotes={quotes} changerUser={changerUser} index={index} colors={colors} randomColor={randomColor} />
+      </div>
     </div>
   )
 }
